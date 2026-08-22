59a194be815194433c65e41243ee7226df17a202
123juditt <judittantony@gmail.com>
2026-08-22 21:33:14 +0530

    Add colour.java to manage a list of colors

---
+++ tutorial 5/colour.java
@@ -0,0 +1,17 @@
+package colours;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector<String> colors = new Vector<>();
+
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+
+        colors.remove("Green");
+
+        System.out.println(colors);
+    }
+}
